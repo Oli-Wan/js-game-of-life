1fcc353896aec443ced657023c5339bab01cdf3d
Olivier Schneider <olivier.schneider@opengroupware.ch>
2014-02-08 12:55:10 +0100

    Fix zooming

--- game.js
+++ game.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
     window.focus = {};
     window.focus.x = 300;
     window.focus.y = 300;
+
+    window.xMinRange = 0;
+    window.xMaxRange = 600;
+    window.yMinRange = 0;
+    window.yMaxRange = 600;
+
     init();
     displayScale();
 
@@ -15,6 +21,8 @@ $(document).ready(function(){
     $("#next").click(oneGeneration);
     $("#zoomin").click(zoom);
     $("#zoomout").click(dezoom);
+
+    //$("#game-canvas").click(setFocus)
 });
 
 function init(){
@@ -60,6 +68,7 @@ function oneGeneration(){
 
 function zoom(){
     window.scale++;
+    calculateZomRange();
     displayScale();
     restoreState();
 }
@@ -69,42 +78,28 @@ function dezoom(){
         return;
 
     window.scale--;
+    calculateZomRange();
     displayScale();
     restoreState();
 }
 
-function displayScale() {
-    $("#scale").html( window.scale );
-}
-
-function restoreState(){
-    var width = window.width;
-    var height = window.height;
-    var gameBoard = window.gameBoard;
-    var refTable = window.generationCounter%2;
-    var gameCanvas = document.getElementById("game-canvas");
-    var b_context = gameCanvas.getContext("2d");
-
+function calculateZomRange() {
     var scale = window.scale;
-    var heightInPixel = height/2;
-    var widthInPixel = width/2;
+
+    var scaledWidth = Math.abs(window.width/scale/2);
+    var scaledHeight = Math.abs(window.height/scale/2);
 
     var xFocus = window.focus.x;
     var yFocus = window.focus.y;
 
-    for(var i = 0; i < height; i++){
-        for(var j = 0; j < width; j++){
-            if((i >= (xFocus - heightInPixel)) && (i <= (xFocus + heightInPixel))
-                && (j >= (yFocus - widthInPixel)) && (j <= (yFocus + widthInPixel)))
-            {
-                if(gameBoard[i][j][refTable])
-                    b_context.fillRect(i*scale, j*scale, 1*scale, 1*scale);
-                else
-                    b_context.clearRect(i*scale, j*scale, 1*scale, 1*scale);
-            }
-        }
-    }
-    showFocus(b_context);
+    window.xMinRange = xFocus - scaledWidth;
+    window.xMaxRange = xFocus + scaledWidth;
+    window.yMinRange = yFocus - scaledHeight;
+    window.yMaxRange = yFocus + scaledHeight;
+}
+
+function displayScale() {
+    $("#scale").html( window.scale );
 }
 
 function showFocus(b_context) {
@@ -116,8 +111,57 @@ function showFocus(b_context) {
     b_context.fillStyle = "black";
 }
 
+
+function setFocus(e) {
+    var x = Math.floor(e.pageX-$("#game-canvas").offset().left);
+    var y = Math.floor(e.pageY-$("#game-canvas").offset().top);
+
+    window.focus.x = x;
+    window.focus.y = y;
+
+    restoreState();
+}
+
+function restoreState(){
+    var width = window.width;
+    var height = window.height;
+    var gameBoard = window.gameBoard;
+    var refTable = window.generationCounter%2;
+    var gameCanvas = document.getElementById("game-canvas");
+    var drawer = gameCanvas.getContext("2d");
+
+    var scale = window.scale;
+
+    var xMinRange = window.xMinRange;
+    var xMaxRange = window.xMaxRange;
+    var yMinRange = window.yMinRange;
+    var yMaxRange = window.yMaxRange;
+
+    for(var y = 0; y < height; y++){
+        for(var x = 0; x < width; x++) {
+
+            if (gameBoard[y][x][refTable])
+                drawer.fillRect((x - xMinRange)*scale , (y- yMinRange)*scale , scale, scale);
+            else
+                drawer.clearRect((x - xMinRange)*scale , (y- yMinRange)*scale, scale, scale);
+
+//            display(x, xMinRange, xMaxRange, y, yMinRange, yMaxRange, gameBoard[y][x][refTable], drawer, scale);
+        }
+    }
+    showFocus(drawer);
+}
+
+function display(x, xMinRange, xMaxRange, y, yMinRange, yMaxRange, alive, drawer, scale) {
+    if ((x > xMinRange) && (x < xMaxRange) && (y > yMinRange) && (y < yMaxRange)) {
+        if (alive)
+            drawer.fillRect((x - xMinRange)*scale , (y- yMinRange)*scale , scale, scale);
+        else
+            drawer.clearRect((x - xMinRange)*scale , (y- yMinRange)*scale, scale, scale);
+    }
+}
+
 function nextState(){
-    var generationNb = ++window.generationCounter
+    var generationNb = ++window.generationCounter;
     var newTable = window.generationCounter%2;
     var refTable = ( newTable == 0 ) ? 1:0;
 
@@ -131,75 +175,67 @@ function nextState(){
     var gameBoard = window.gameBoard;
 
     var scale = window.scale;
-    var heightInPixel = height/2;
-    var widthInPixel = width/2;
 
-    var xFocus = window.focus.x;
-    var yFocus = window.focus.y;
+    var xMinRange = window.xMinRange;
+    var xMaxRange = window.xMaxRange;
+    var yMinRange = window.yMinRange;
+    var yMaxRange = window.yMaxRange;
 
-    for(var i = 0; i < height; i++){
-        for(var j = 0; j < width; j++){
+    for(var y = 0; y < height; y++){
+        for(var x = 0; x < width; x++){
 
-            var neighboorCellsNb = 0;
+            var neighbourCellsNb = 0;
 
-            if(i > 0 )
+            if(y > 0 )
             {
-                if(gameBoard[i-1][j][refTable])
-                    neighboorCellsNb++;
+                if(gameBoard[y-1][x][refTable])
+                    neighbourCellsNb++;
 
-                if( j > 1)
-                    if(gameBoard[i-1][j-1][refTable])
-                        neighboorCellsNb++;
-                if(j < height-1)
-                    if(gameBoard[i-1][j+1][refTable])
-                        neighboorCellsNb++;
+                if( x > 1)
+                    if(gameBoard[y-1][x-1][refTable])
+                        neighbourCellsNb++;
+                if(x < height-1)
+                    if(gameBoard[y-1][x+1][refTable])
+                        neighbourCellsNb++;
             }
 
-            if(i < height-1 )
+            if(y < height-1 )
             {
-                if(gameBoard[i+1][j][refTable])
-                    neighboorCellsNb++;
+                if(gameBoard[y+1][x][refTable])
+                    neighbourCellsNb++;
 
-                if( j > 1)
-                    if(gameBoard[i+1][j-1][refTable])
-                        neighboorCellsNb++;
-                if(j < height-1)
-                    if(gameBoard[i+1][j+1][refTable])
-                        neighboorCellsNb++;
+                if( x > 1)
+                    if(gameBoard[y+1][x-1][refTable])
+                        neighbourCellsNb++;
+                if(x < height-1)
+                    if(gameBoard[y+1][x+1][refTable])
+                        neighbourCellsNb++;
             }
 
-            if( j > 0)
-                if(gameBoard[i][j-1][refTable])
-                    neighboorCellsNb++;
+            if( x > 0)
+                if(gameBoard[y][x-1][refTable])
+                    neighbourCellsNb++;
 
-            if(j < height-1)
-                if(gameBoard[i][j+1][refTable])
-                    neighboorCellsNb++;
+            if(x < height-1)
+                if(gameBoard[y][x+1][refTable])
+                    neighbourCellsNb++;
 
-            var currentState = gameBoard[i][j][refTable];
+            var currentState = gameBoard[y][x][refTable];
             var newState = currentState;
 
-            if(neighboorCellsNb < 2 && currentState)
+            if(neighbourCellsNb < 2 && currentState)
                 newState = false;
 
-            if(neighboorCellsNb > 3 && currentState)
+            if(neighbourCellsNb > 3 && currentState)
                 newState = false;
 
-            if(neighboorCellsNb == 3 && !currentState )
+            if(neighbourCellsNb == 3 && !currentState )
                 newState = true;
 
-            gameBoard[i][j][newTable] = newState;
+            gameBoard[y][x][newTable] = newState;
 
-            if((i >= (xFocus - heightInPixel)) && (i <= (xFocus + heightInPixel))
-                && (j >= (yFocus - widthInPixel)) && (j <= (yFocus + widthInPixel)))
-            {
-                if(newState != currentState)
-                {
-                    if(newState)
-                        b_context.fillRect(i*scale, j*scale, 1*scale, 1*scale);
-                    else
-                        b_context.clearRect(i*scale, j*scale, 1*scale, 1*scale);
-                }
+            if (newState != currentState) {
+                display(x, xMinRange, xMaxRange, y, yMinRange, yMaxRange, newState,  b_context, scale);
             }
         }
     }
