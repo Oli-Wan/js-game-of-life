845aca6fb5f08da6d6f2af00d7369b96529cad93
Olivier Schneider <olivier.schneider@opengroupware.ch>
2014-03-17 21:58:53 +0100

    Structure the code in objects

--- content/scripts/game.js
+++ content/scripts/game.js
@@ -1,241 +1,241 @@
 $(document).ready(function(){
-    window.width = 600;
-    window.height = 600;
-    window.scale = 1;
-
-    window.focus = {};
-    window.focus.x = 300;
-    window.focus.y = 300;
-
-    window.xMinRange = 0;
-    window.xMaxRange = 600;
-    window.yMinRange = 0;
-    window.yMaxRange = 600;
-
-    init();
-    displayScale();
-
-    //attaching events
-    $("#start").click(startGame);
-    $("#stop").click(stopGame);
-    $("#next").click(oneGeneration);
-    $("#zoomin").click(zoom);
-    $("#zoomout").click(dezoom);
-});
-
-function init(){
-    window.generationCounter = 0;
-    var width = window.width;
-    var height = window.height;
+    var width = 600,
+        height = 600;
 
     var canvas = document.getElementById("game-canvas");
 
-    var gameBoard = new Array(height);
+    var drawer = new Drawer(width, height, canvas.getContext("2d"));
+    var game = new GameOfLife(width, height, drawer);
+
+    //attaching events
+    $("#start").click(function(){
+        window.run = setInterval(game.nextGeneration, 50);
+        $("#start").attr("disabled", "disabled");
+    });
+
+    $("#stop").click(function() {
+        clearInterval(window.run);
+        $("#start").removeAttr("disabled");
+    });
+
+    $("#next").click(function() {
+        var date1 = new Date();
+        game.nextGeneration();
+        var date2 = new Date();
+        $("#info").html( "Generation time : " + (date2 - date1) );
+    });
+
+    $("#zoomin").click(function(){
+        drawer.zoom();
+        game.restoreState();
+    });
+
+    $("#zoomout").click(function(){
+        drawer.dezoom();
+        game.restoreState();
+    });
+});
+
+function GameOfLife(width, height, drawer) {
+    var self = this;
+
+    self.scale = 1;
+    self.width = width;
+    self.height = height;
+    self.generation = 0;
+
+    self.board = new Array(height);
+    self.drawer = drawer;
+
 
     for(var i = 0; i < height; i++){
         var newRow = new Array(width);
-
         for(var j = 0; j < width; j++)
-        {
-            var random = Math.random() > 0.5;
-            newRow[j] = [random, random];
-        }
-        gameBoard[i] = newRow;
+            newRow[j] = Math.random() > 0.5;
+
+        self.board[i] = newRow;
     }
-    window.gameBoard = gameBoard;
 
-    nextState();
+    self.nextGeneration = function() {
+        self.generation += 1;
+
+        var width = self.width,
+            height = self.height;
+
+        var newGeneration = [];
+
+        for(var y = 0; y < height; y++){
+            var newRow = [];
+
+            for(var x = 0; x < width; x++){
+
+                var neighbourCount = 0;
+
+                if(y > 0 )
+                {
+                    if(self.board[y-1][x])
+                        neighbourCount++;
+
+                    if( x > 1)
+                        if(self.board[y-1][x-1])
+                            neighbourCount++;
+                    if(x < height-1)
+                        if(self.board[y-1][x+1])
+                            neighbourCount++;
+                }
+
+                if(y < height-1 )
+                {
+                    if(self.board[y+1][x])
+                        neighbourCount++;
+
+                    if( x > 1)
+                        if(self.board[y+1][x-1])
+                            neighbourCount++;
+                    if(x < height-1)
+                        if(self.board[y+1][x+1])
+                            neighbourCount++;
+                }
+
+                if( x > 0)
+                    if(self.board[y][x-1])
+                        neighbourCount++;
+
+                if(x < height-1)
+                    if(self.board[y][x+1])
+                        neighbourCount++;
+
+                var currentState = self.board[y][x];
+                var newState = currentState;
+
+                if(neighbourCount < 2 && currentState)
+                    newState = false;
+
+                if(neighbourCount > 3 && currentState)
+                    newState = false;
+
+                if(neighbourCount == 3 && !currentState )
+                    newState = true;
+
+                newRow[x] = newState;
+
+                if (newState != currentState) {
+                    self.drawer.display(x, y, newState);
+                }
+            }
+            newGeneration[y] = newRow;
+        }
+
+        self.board = newGeneration;
+        self.drawer.showFocus();
+    };
+
+    self.restoreState = function() {
+        for(var y = 0; y < self.height; y++){
+            for(var x = 0; x < self.width; x++) {
+                if (self.board[y][x])
+                    self.drawer.fill(x,y);
+                else
+                    self.drawer.clear(x,y);
+            }
+        }
+        self.drawer.showFocus();
+    };
+
+    self.restoreState();
 }
 
-function startGame(){
-    window.run = setInterval(nextState, 50);
-    $("#start").attr("disabled", "disabled");
-}
+function Drawer(width, height, canvas) {
+    var self = this;
 
-function stopGame(){
-    clearInterval(window.run);
-    $("#start").removeAttr("disabled");
-}
+    self.canvas = canvas;
+    self.width = width;
+    self.height = height;
 
-function oneGeneration(){
-    var date1 = new Date();
-    nextState();
-    var date2 = new Date();
-    $("#info").html( "Generation time : " + (date2 - date1) );
-}
+    self.scale = 1;
+    self.focus = {
+        x: height/2,
+        y: width/2
+    };
+    self.range = {
+        xMax: width,
+        xMin:0,
+        yMax: height,
+        yMin:0
+    };
 
-function zoom(){
-    window.scale++;
-    calculateZomRange();
-    displayScale();
-    restoreState();
-}
+    self.zoom = function() {
+        self.scale += 1;
+        self.calculateRange();
+        self.displayScale();
+    };
 
-function dezoom(){
-    if(window.scale == 1)
-        return;
+    self.dezoom = function() {
+        if(self.scale == 1)
+            return;
 
-    window.scale--;
-    calculateZomRange();
-    displayScale();
-    restoreState();
-}
+        self.scale -= 1;
+        self.calculateRange();
+        self.displayScale();
+    };
 
-function calculateZomRange() {
-    var scale = window.scale;
+    self.calculateRange = function() {
+        var scaledWidth = Math.abs(self.width/self.scale/2);
+        var scaledHeight = Math.abs(self.height/self.scale/2);
 
-    var scaledWidth = Math.abs(window.width/scale/2);
-    var scaledHeight = Math.abs(window.height/scale/2);
+        var xFocus = self.focus.x;
+        var yFocus = self.focus.y;
 
-    var xFocus = window.focus.x;
-    var yFocus = window.focus.y;
+        self.range = {
+            xMax:xFocus + scaledWidth,
+            xMin:xFocus - scaledWidth,
+            yMax:yFocus + scaledHeight,
+            yMin:yFocus - scaledHeight
+        };
+    };
 
-    window.xMinRange = xFocus - scaledWidth;
-    window.xMaxRange = xFocus + scaledWidth;
-    window.yMinRange = yFocus - scaledHeight;
-    window.yMaxRange = yFocus + scaledHeight;
-}
+    self.isInRange = function(x,y) {
+        return (x > self.range.xMin) && (x < self.range.xMax) && (y > self.range.yMin) && (y < self.range.yMax)
+    };
 
-function displayScale() {
-    $("#scale").html( window.scale );
-}
+    self.displayScale = function() {
+        $("#scale").html( self.scale );
+    };
 
-function showFocus(b_context) {
-    var xFocus = window.focus.x;
-    var yFocus = window.focus.y;
-    b_context.fillStyle = "rgba(200, 0, 0, 0.5)";
-    b_context.fillRect(xFocus, yFocus-10, 5, 25);
-    b_context.fillRect(xFocus-10, yFocus, 25, 5);
-    b_context.fillStyle = "black";
-}
+    self.translateX = function(x) {
+        return x - self.range.xMin;
+    };
 
+    self.translateY = function(x) {
+        return x - self.range.yMin;
+    };
 
-function setFocus(e) {
-    var x = Math.floor(e.pageX-$("#game-canvas").offset().left);
-    var y = Math.floor(e.pageY-$("#game-canvas").offset().top);
+    self.showFocus = function() {
+        var xFocus = self.focus.x;
+        var yFocus = self.focus.y;
+        self.canvas.fillStyle = "rgba(200, 0, 0, 0.5)";
+        self.canvas.fillRect(xFocus, yFocus-10, 5, 25);
+        self.canvas.fillRect(xFocus-10, yFocus, 25, 5);
+        self.canvas.fillStyle = "black";
+    };
 
-    window.focus.x = x;
-    window.focus.y = y;
-
-    restoreState();
-}
-
-function restoreState(){
-    var width = window.width;
-    var height = window.height;
-    var gameBoard = window.gameBoard;
-    var refTable = window.generationCounter%2;
-    var gameCanvas = document.getElementById("game-canvas");
-    var drawer = gameCanvas.getContext("2d");
-
-    var scale = window.scale;
-
-    var xMinRange = window.xMinRange;
-    var xMaxRange = window.xMaxRange;
-    var yMinRange = window.yMinRange;
-    var yMaxRange = window.yMaxRange;
-
-    for(var y = 0; y < height; y++){
-        for(var x = 0; x < width; x++) {
-
-            if (gameBoard[y][x][refTable])
-                drawer.fillRect((x - xMinRange)*scale , (y- yMinRange)*scale , scale, scale);
+    self.display = function(x,  y, alive) {
+        if(self.isInRange(x,y)) {
+            if(alive)
+                self.fill(x, y);
             else
-                drawer.clearRect((x - xMinRange)*scale , (y- yMinRange)*scale, scale, scale);
-
-//            display(x, xMinRange, xMaxRange, y, yMinRange, yMaxRange, gameBoard[y][x][refTable], drawer, scale);
+                self.clear(x, y);
         }
-    }
-    showFocus(drawer);
+    };
+
+    self.fill = function(x, y) {
+        var scale = self.scale;
+        self.canvas.fillRect((x - self.range.xMin)*scale , (y - self.range.yMin)*scale , scale, scale);
+    };
+
+    self.clear = function(x, y) {
+        var scale = self.scale;
+        self.canvas.clearRect((x - self.range.xMin)*scale , (y - self.range.yMin)*scale , scale, scale);
+    };
 }
 
-function display(x, xMinRange, xMaxRange, y, yMinRange, yMaxRange, alive, drawer, scale) {
-    if ((x > xMinRange) && (x < xMaxRange) && (y > yMinRange) && (y < yMaxRange)) {
-        if (alive)
-            drawer.fillRect((x - xMinRange)*scale , (y- yMinRange)*scale , scale, scale);
-        else
-            drawer.clearRect((x - xMinRange)*scale , (y- yMinRange)*scale, scale, scale);
-    }
-}
-
-function nextState(){
-    var generationNb = ++window.generationCounter;
-    var newTable = window.generationCounter%2;
-    var refTable = ( newTable == 0 ) ? 1:0;
-
-    var generationNbP = $("#generation-nb");
-    generationNbP.html( generationNb );
-
-    var gameCanvas = document.getElementById("game-canvas");
-    var b_context = gameCanvas.getContext("2d");
-    var width = window.width;
-    var height = window.height;
-    var gameBoard = window.gameBoard;
-
-    var scale = window.scale;
-
-    var xMinRange = window.xMinRange;
-    var xMaxRange = window.xMaxRange;
-    var yMinRange = window.yMinRange;
-    var yMaxRange = window.yMaxRange;
-
-    for(var y = 0; y < height; y++){
-        for(var x = 0; x < width; x++){
-
-            var neighbourCellsNb = 0;
-
-            if(y > 0 )
-            {
-                if(gameBoard[y-1][x][refTable])
-                    neighbourCellsNb++;
-
-                if( x > 1)
-                    if(gameBoard[y-1][x-1][refTable])
-                        neighbourCellsNb++;
-                if(x < height-1)
-                    if(gameBoard[y-1][x+1][refTable])
-                        neighbourCellsNb++;
-            }
-
-            if(y < height-1 )
-            {
-                if(gameBoard[y+1][x][refTable])
-                    neighbourCellsNb++;
-
-                if( x > 1)
-                    if(gameBoard[y+1][x-1][refTable])
-                        neighbourCellsNb++;
-                if(x < height-1)
-                    if(gameBoard[y+1][x+1][refTable])
-                        neighbourCellsNb++;
-            }
-
-            if( x > 0)
-                if(gameBoard[y][x-1][refTable])
-                    neighbourCellsNb++;
-
-            if(x < height-1)
-                if(gameBoard[y][x+1][refTable])
-                    neighbourCellsNb++;
-
-            var currentState = gameBoard[y][x][refTable];
-            var newState = currentState;
-
-            if(neighbourCellsNb < 2 && currentState)
-                newState = false;
-
-            if(neighbourCellsNb > 3 && currentState)
-                newState = false;
-
-            if(neighbourCellsNb == 3 && !currentState )
-                newState = true;
-
-            gameBoard[y][x][newTable] = newState;
-
-            if (newState != currentState) {
-                display(x, xMinRange, xMaxRange, y, yMinRange, yMaxRange, newState,  b_context, scale);
-            }
-        }
-    }
-    showFocus(b_context);
-}
